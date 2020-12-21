ed96e9a7b5f20357c4d56993101c41fca52a9c1c
ValentineLgd <valentinelagand@gmail.com>
2020-12-21 09:44:33 +0000

    Implement emailsJS integration on contact page

---
+++ assets/js/emails.js
@@ -0,0 +1,17 @@
+function sendMail(contactForm) {
+    emailjs.send("service_7qovzsn","BoliviAround", {
+        "from_name": contactForm.name.value,
+        "from_surname": contactForm.surname.value,
+        "from_email": contactForm.emailaddress.value,
+        "message": contactForm.message.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false;
+}
