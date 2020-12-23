91382342ab28c553a4c73820ab9b3495ec24b176
ValentineLgd <valentinelagand@gmail.com>
2020-12-23 10:58:43 +0000

    Add JQuery to make activities sections hide/show when clicking on buttons

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,9 +1,9 @@
 function initMap() {
-                var map = new google.maps.Map(document.getElementById("map"), {
-                    zoom: 7,
-                    center: {
-                        lat: -20.072305495345528,
-                        lng: -67.7087008695497
+            var map = new google.maps.Map(document.getElementById("map"), {
+                zoom: 7,
+                center: {
+                    lat: -20.072305495345528,
+                    lng: -67.7087008695497
                     }
                 });
             
@@ -23,3 +23,17 @@ function initMap() {
                 imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'
             });
         }
+
+
+        
+  const locations = [
+    ["Salar d'Uyuni", -20.012628599701625, -67.81195422417898],
+    ["Yungaz Road", -16.264692546146286, -67.78893702608976],
+    ["Eduardo Avaroa", -18.890052731395137, -66.75225678903733],
+    ["Gustu", -16.5445410118336, -68.08655004495911],
+    ["Choriceria", -19.045374104018236, -65.25827944562785],
+    ["El Huerto", -19.037201903024396, -65.2622114744638],
+    ["Kultur", -19.049729028381368, -65.25705575912029],
+    ["Bymz Bar", -17.785521446880388, -63.180204218666496],
+    ["Diesel Nacional", -16.508669717197826, -68.12765094495937],
+  ];
---
+++ assets/js/script.js
@@ -0,0 +1,149 @@
+
+  $(".description-salar-duyuni").show();
+  $(".description-gustu").hide();
+  $(".description-choriceria").hide();
+  $(".description-elhuerto").hide();
+  $(".description-yungaz-road").hide();
+  $(".description-eduardo-avaroa").hide();
+  $(".description-bymz").hide();
+  $(".description-kultur").hide();
+  $(".description-diesel").hide();
+
+/**Adventure section */
+$("#btn-adventure").click(function(){
+  $(".description-salar-duyuni").show();
+  $(".description-gustu").hide();
+  $(".description-choriceria").hide();
+  $(".description-elhuerto").hide();
+  $(".description-yungaz-road").hide();
+  $(".description-bymz").hide();
+  $(".description-kultur").hide();
+  $(".description-diesel").hide();
+  $(".description-eduardo-avaroa").hide();});
+
+/**2) Small buttons */
+
+$(".btn-salar").click(function(){
+  $(".description-salar-duyuni").show();
+  $(".description-gustu").hide();
+  $(".description-choriceria").hide();
+  $(".description-elhuerto").hide();
+  $(".description-yungaz-road").hide();
+  $(".description-bymz").hide();
+  $(".description-kultur").hide();
+  $(".description-diesel").hide();
+  $(".description-eduardo-avaroa").hide();});
+
+$(".btn-eduardo").click(function(){
+  $(".description-salar-duyuni").hide();
+  $(".description-gustu").hide();
+  $(".description-choriceria").hide();
+  $(".description-elhuerto").hide();
+  $(".description-yungaz-road").hide();
+  $(".description-bymz").hide();
+  $(".description-kultur").hide();
+  $(".description-diesel").hide();
+  $(".description-eduardo-avaroa").show();});
+
+$(".btn-yungaz").click(function(){
+  $(".description-salar-duyuni").hide();
+  $(".description-gustu").hide();
+  $(".description-choriceria").hide();
+  $(".description-elhuerto").hide();
+  $(".description-yungaz-road").show();
+  $(".description-bymz").hide();
+  $(".description-kultur").hide();
+  $(".description-diesel").hide();
+  $(".description-eduardo-avaroa").hide();});
+
+  /**Food section */
+
+$("#btn-food").click(function(){
+  $(".description-salar-duyuni").hide();
+  $(".description-gustu").show();
+  $(".description-choriceria").hide();
+  $(".description-elhuerto").hide();
+  $(".description-yungaz-road").hide();
+  $(".description-bymz").hide();
+  $(".description-kultur").hide();
+  $(".description-diesel").hide();
+  $(".description-eduardo-avaroa").hide();});
+
+$(".btn-gustu").click(function(){
+  $(".description-salar-duyuni").hide();
+  $(".description-gustu").show();
+  $(".description-choriceria").hide();
+  $(".description-elhuerto").hide();
+  $(".description-yungaz-road").hide();
+  $(".description-bymz").hide();
+  $(".description-kultur").hide();
+  $(".description-diesel").hide();
+  $(".description-eduardo-avaroa").hide();});
+
+$(".btn-choriceria").click(function(){
+  $(".description-salar-duyuni").hide();
+  $(".description-gustu").hide();
+  $(".description-choriceria").show();
+  $(".description-elhuerto").hide();
+  $(".description-yungaz-road").hide();
+  $(".description-bymz").hide();
+  $(".description-kultur").hide();
+  $(".description-diesel").hide();
+  $(".description-eduardo-avaroa").hide();});
+
+$(".btn-elhuerto").click(function(){
+  $(".description-salar-duyuni").hide();
+  $(".description-gustu").hide();
+  $(".description-choriceria").hide();
+  $(".description-elhuerto").show();
+  $(".description-yungaz-road").hide();
+  $(".description-bymz").hide();
+  $(".description-kultur").hide();
+  $(".description-diesel").hide();
+  $(".description-eduardo-avaroa").hide();});
+
+/** Party section */
+
+$("#btn-party").click(function(){
+  $(".description-salar-duyuni").hide();
+  $(".description-gustu").hide();
+  $(".description-choriceria").hide();
+  $(".description-elhuerto").hide();
+  $(".description-yungaz-road").hide();
+  $(".description-bymz").hide();
+  $(".description-kultur").hide();
+  $(".description-diesel").show();
+  $(".description-eduardo-avaroa").hide();});
+
+$(".btn-diesel").click(function(){
+  $(".description-salar-duyuni").hide();
+  $(".description-gustu").hide();
+  $(".description-choriceria").hide();
+  $(".description-elhuerto").hide();
+  $(".description-yungaz-road").hide();
+  $(".description-bymz").hide();
+  $(".description-kultur").hide();
+  $(".description-diesel").show();
+  $(".description-eduardo-avaroa").hide();});
+
+$(".btn-kultur").click(function(){
+  $(".description-salar-duyuni").hide();
+  $(".description-gustu").hide();
+  $(".description-choriceria").hide();
+  $(".description-elhuerto").hide();
+  $(".description-yungaz-road").hide();
+  $(".description-bymz").hide();
+  $(".description-kultur").show();
+  $(".description-diesel").hide();
+  $(".description-eduardo-avaroa").hide();});
+
+$(".btn-bymz").click(function(){
+  $(".description-salar-duyuni").hide();
+  $(".description-gustu").hide();
+  $(".description-choriceria").hide();
+  $(".description-elhuerto").hide();
+  $(".description-yungaz-road").hide();
+  $(".description-bymz").show();
+  $(".description-kultur").hide();
+  $(".description-diesel").hide();
+  $(".description-eduardo-avaroa").hide();});
